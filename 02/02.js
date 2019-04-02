28ffb99b4043dda07ceb30e0c45cb34e14e858d8
Diego <DiegoRoyal@qq.com>
2019-04-02 13:36:19 +0800

    finish

--- 02/02.js
+++ 02/02.js
@@ -6,6 +6,8 @@ var points = [];
 
 var NumTimesToSubdivide = 5;
 
+var ddapoints = 0;
+
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
@@ -21,6 +23,7 @@ window.onload = function init()
     
     initF();
     points.push(0,0,20,8);
+    lineDDA(0,0,20,8);
     coordTrans();
     //
     //  Configure WebGL
@@ -48,6 +51,25 @@ window.onload = function init()
     render();
 }
 
+function lineDDA( x0, y0, xEnd, yEnd){
+    var dx=xEnd-x0,dy=yEnd-y0,steps,k;
+	var xIncrement,yIncrement,x=x0,y=y0;
+	if(Math.abs(dx)>Math.abs(dy))//确定步长，谁大就取谁
+		steps=Math.abs(dx);
+	else
+		steps=Math.abs(dy);
+	xIncrement=dx/steps;//增量当中有一个会为1，另一个会为斜率k
+	yIncrement=dy/steps;
+    points.push(Math.round(x),Math.round(y));//由于每次都加了小于1的增量，所以需要取整
+    ddapoints++;
+	for(k=0;k<steps;k++){
+		x+=xIncrement;
+		y+=yIncrement;
+        points.push(Math.round(x),Math.round(y));
+        ddapoints++;
+	}
+
+}
 function coordTrans(){
     var i;
     for(i=0;i<points.length;i++){
@@ -69,6 +91,7 @@ function initF()
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.LINES, 0, points.length/2 );
+    gl.drawArrays( gl.LINES, 0, points.length/2-ddapoints );
+    gl.drawArrays( gl.POINTS, points.length/2-ddapoints, ddapoints );
 }
 
