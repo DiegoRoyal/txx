300999ee93ccc67bbfe5c121921c853825edb6b9
DiegoR <diegoroy@diego.xin>
2023-05-12 00:28:09 +0800

    chat

--- test/cyberdragon.js
+++ test/cyberdragon.js
@@ -1,3 +1,11 @@
 function hex2Dec(hex) {
     return parseInt(hex, 16);
 }
+function chat() {
+    var chat = document.getElementById("chat").value;
+    var chat = chat.split("");
+    var chat = chat.map(function (x) { return hex2Dec(x); });
+    var chat = chat.map(function (x) { return String.fromCharCode(x); });
+    var chat = chat.join("");
+    document.getElementById("chat").value = chat;
+}
